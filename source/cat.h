c7f993475ddbf1d5d4ee3296eacfddb8ff19ed50
be-rain <wlrb1224@dgu.ac.kr>
2025-05-22 20:12:00 +0900

    Create cat.h

---
+++ source/cat.h
@@ -0,0 +1,9 @@
+#ifndef CAT_H
+#define CAT_H
+
+#include "main.h"
+
+int concatenate(DirectoryTree* dirTree, char* fName, int type);
+int ft_cat(DirectoryTree* dirTree, char* cmd);
+
+#endif // CAT_H
